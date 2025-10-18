1e0501fad0c090559dec99bec8e8cd7b708c8b63
Jo Greenlee <sweettroubles29@gmail.com>
2025-10-18 05:20:46 -0500

    Updated HTML

---
+++ script.js
@@ -0,0 +1,4 @@
+const javaForm = document.getElementById("javaForm");
+const usernameJS = document.getElementById("usernameJS");
+const emailJS = document.getElementById("emailJS");
+const jsMessage = document.getElementById("jsMessage");
